8f2f7560cf08beeef98f2072fc278768c55911f5
Nico De Witte <nico.dewitte@vives.be>
2015-02-12 14:38:57 +0100

    Add options menu to main activity and place settings as an option in the menu instead of the button on the main activity

--- src/be/vives/thumper/MainActivity.java
+++ src/be/vives/thumper/MainActivity.java
@@ -3,6 +3,9 @@ package be.vives.thumper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -17,11 +20,26 @@ public class MainActivity extends Activity {
     	startActivity(new Intent(this, ThumperControlActivity.class));
     }
     
-    public void onSettings(View v) {
-    	startActivity(new Intent(this, AppPreferenceActivity.class));
-    }
-    
     public void onOrientation(View v) {
     	startActivity(new Intent(this, OrientationControlActivity.class));
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.show_settings:
+                startActivity(new Intent(this, AppPreferenceActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
