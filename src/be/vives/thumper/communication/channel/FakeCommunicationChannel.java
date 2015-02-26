157b57e93680beda6e965b99f8fdbcb78d06b534
Nico De Witte <nico.dewitte@vives.be>
2015-02-26 18:21:53 +0100

    Add fake comm channel for testing

---
+++ src/be/vives/thumper/communication/channel/FakeCommunicationChannel.java
@@ -0,0 +1,36 @@
+package be.vives.thumper.communication.channel;
+
+import android.content.Context;
+import be.vives.thumper.trex.IThumperStatusReady;
+import be.vives.thumper.trex.ThumperCommand;
+
+public class FakeCommunicationChannel extends ThumperCommunicationChannel {
+
+	@Override
+	public void getThumperStatus(Context context, IThumperStatusReady callback) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void sendThumperCommand(Context context, ThumperCommand command,
+			IThumperStatusReady callback) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean isPersistent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+	}
+}
